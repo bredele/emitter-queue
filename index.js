5bf46983d2b4d3b6fb1ebd114cc9e562fcd32a0d
bredele <olivier.wietrich@gmail.com>
2014-03-17 13:15:15 -0600

    change scope

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ function Queue(emitter) {
     this._queue = this._queue || {};
     this._callbacks = this._callbacks || {};
     if(this._callbacks[topic]) {
-      this.emit.apply(emitter, arguments);
+      this.emit.apply(this, arguments);
     } else {
       (this._queue[topic] = this._queue[topic] || [])
         .push([].slice.call(arguments, 1));
@@ -59,10 +59,10 @@ function Queue(emitter) {
   emitter.on = emitter.addEventListener = function(topic, fn) {
     this._queue = this._queue || {};
     var topics = this._queue[topic];
-    cache.apply(emitter, arguments);
+    cache.apply(this, arguments);
     if(topics) {
       for(var i = 0, l = topics.length; i < l; i++) {
-        fn.apply(emitter, topics[i]);
+        fn.apply(this, topics[i]);
       }
       delete this._queue[topic];
     }
